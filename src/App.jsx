32351d3220b9e54244aeae497727c65f17f0407c
DenHax <whatxpw@gmail.com>
2025-01-19 21:52:13 +0300

    Refactoring app for move pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Layout } from "antd";
-import UploadEntity from "./components/Entities/Upload/UploadEntity";
+import Navbar from "./components/Navbar/Navside";
+import UploadPage from "./pages/UploadPage";
+import EmployeePage from "./pages/EmployeePage";
+import FuelRoadPage from "./pages/FuelRoadPage";
+import SubsystemPage from "./pages/SubsystemPage";
+
+const { Content, Sider } = Layout;
 
 const App = () => {
   return (
-    <>
-      <h1>APW client</h1>
+    <Router>
       <Layout style={{ minHeight: "100vh" }}>
-        <UploadEntity />
+        <Sider>
+          <Navbar />
+        </Sider>
+        <Layout>
+          <Content style={{ padding: "24px" }}>
+            <Routes>
+              <Route path="/uploads" element={<UploadPage />} />
+              <Route path="/employees" element={<EmployeePage />} />
+              <Route path="/fuel-roads" element={<FuelRoadPage />} />
+              <Route path="/subsystems" element={<SubsystemPage />} />
+              <Route path="*" element={<UploadPage />} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </>
+    </Router>
   );
 };
 
 export default App;
+//
